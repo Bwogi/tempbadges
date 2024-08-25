058c71df0c8c60fe066a559f3d7a2260381b8353
Andrew Bwogi <andrew.bwogi@gmail.com>
2024-08-25 00:21:55 -0700

    check

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import List from "../components/employee-list";
+import List from "../components/employee-list-user";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import EmployeeForm from "../components/employee-form";
 
 const Employees = () => {
   return (
     <div className="px-5 mt-10">
       <div>
-        <Link href="/">
+        <h1 className="text-center text-2xl mb-10">Employee List</h1>
+        <List />
+        {/* <Link href="/">
           <Button>Add Employee</Button>
-        </Link>
+        </Link> */}
       </div>
-      <List />
+      {/* <List />s */}
     </div>
   );
 };
